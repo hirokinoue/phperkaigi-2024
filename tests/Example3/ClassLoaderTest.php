9fad516e41d97d522d43a16d230603f72ad738e7
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2024-02-11 23:40:41 +0900

    test: コードの変更に合わせてテストの入力と想定を見直す

--- tests/Example3/ClassLoaderTest.php
+++ tests/Example3/ClassLoaderTest.php
@@ -3,6 +3,7 @@
 namespace Hirokinoue\DependencyVisualizer\Tests\Example3;
 
 use Hirokinoue\DependencyVisualizer\Example3\ClassLoader;
+use PhpParser\Node\Name\FullyQualified;
 use PHPUnit\Framework\TestCase;
 
 final class ClassLoaderTest extends TestCase
@@ -11,23 +12,23 @@ final class ClassLoaderTest extends TestCase
      * @dataProvider data対象に応じてロードできること
      * @noinspection NonAsciiCharacters
      */
-    public function test対象に応じてロードできること(string $qualifiedName, string $expectedQualifiedClassName, string $expectedCode): void
+    public function test対象に応じてロードできること(FullyQualified $fullyQualified, string $expectedClassName, string $expectedCode): void
     {
         // given
-        $sut = ClassLoader::create($qualifiedName);
+        $sut = ClassLoader::create($fullyQualified);
 
         // when
         $className = $sut->className();
         $code = $sut->content();
 
         // then
-        $this->assertSame($expectedQualifiedClassName, $className);
+        $this->assertSame($expectedClassName, $className);
         $this->assertSame($expectedCode, $code);
     }
 
     /**
      * @noinspection NonAsciiCharacters
-     * @return array<string, array<int, string>>
+     * @return array<string, array<int, string|FullyQualified>>
      */
     public function data対象に応じてロードできること(): array
     {
@@ -39,22 +40,22 @@ class UserDefinedClass{}
 CODE;
         return [
             'ユーザー定義クラスはクラス名とコードが取得できる' => [
-                'Hirokinoue\DependencyVisualizer\Tests\Example3\data\UserDefinedClass',
-                'Hirokinoue\DependencyVisualizer\Tests\Example3\data\UserDefinedClass',
+                new FullyQualified('Hirokinoue\DependencyVisualizer\Tests\Example3\data\UserDefinedClass'),
+                '\Hirokinoue\DependencyVisualizer\Tests\Example3\data\UserDefinedClass',
                 $userDefinedClass,
             ],
             '内部クラスはクラス名のみ取得できる' => [
-                'Exception',
-                'Exception',
+                new FullyQualified('Exception'),
+                '\Exception',
                 '',
             ],
             '定数はクラス名もコードも取得できない' => [
-                'FOO',
+                new FullyQualified('FOO'),
                 '',
                 '',
             ],
             'キーワードはクラス名もコードも取得できない' => [
-                'true',
+                new FullyQualified('true'),
                 '',
                 '',
             ],
